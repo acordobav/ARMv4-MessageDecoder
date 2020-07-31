cf57188e18ad7cd63aaaa2ed6c872ee7d4688871
Arturo Cordoba <arturocv16@gmail.com>
2020-07-30 19:12:40 -0600

    Update DataPath, soporte para operacion ROR

--- codigo/DataPath.sv
+++ codigo/DataPath.sv
@@ -1,6 +1,7 @@
 module DataPath #(parameter N=13) (input  logic clk, rst, PCSrc, MemtoReg, ALUSrc, RegWrite, 
 					                    input  logic [3:0]  ALUControl,
-											  input  logic [1:0]  ImmSrc, RegSrc,
+											  input  logic [1:0]  ImmSrc, 
+											  input  logic [2:0]  RegSrc,
 											  input  logic [23:0] Instr,
 											  input  logic [31:0] ReadData,
 					                    output logic [3:0]  ALUFlags,
@@ -20,8 +21,8 @@ Mux2 #(N) PCSrcMux(PCSrc, PCPlus4, Result, PC1);
 PC_Register PCReg(clk, rst, 1'b1, PC1, PC);
 full_adder_nb #(N) PCAdder1(PC, 13'd1, 1'b0, PCPlus4, cout1); 
 full_adder_nb #(N) PCAdder2(PCPlus4, 13'd1, 1'b0, PCPlus8, cout2);
-Mux2 #(4) RegSrcMux0(RegSrc[0], Instr[19:16], 4'b1111, RA1);
-Mux2 #(4) RegSrcMux1(RegSrc[1], Instr[3:0], Instr[15:12], RA2);
+Mux4 #(4) RegSrcMux0(RegSrc[1:0], Instr[19:16], 4'b1111, Instr[11:8], 0, RA1);
+Mux2 #(4) RegSrcMux1(RegSrc[2], Instr[3:0], Instr[15:12], RA2);
 RegisterFile #(32) RegFile(clk, rst, RegWrite, RA1, RA2, Instr[15:12], Result, PCPlus8, RD1, RD2);
 Extend Ext(ImmSrc, Instr[23:0], ExtImm);
 Mux2 #(32) SrcBMux(ALUSrc, RD2, ExtImm, SrcB);
