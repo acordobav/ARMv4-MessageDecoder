daa42d6cba68c5ebf49a40c49aabc74c51d27a01
Fagonar96 <fagonar96@gmail.com>
2020-08-03 22:29:10 -0600

    Update CPU v8

--- codigo/DataPath.sv
+++ codigo/DataPath.sv
@@ -9,7 +9,7 @@ module DataPath #(parameter N=6) (input  logic clk, rst, PCSrc, MemtoReg, ALUSrc
 											  output logic [31:0] WriteData, Addr);
 
 // Conexiones internas
-logic [31:0] Result, RD1, RD2, ExtImm, SrcB, ALUResult;
+logic [31:0] Result, RD1, RD2, ExtImm, SrcB, ALUResult, PCPlus8E;
 logic cout1, cout2;
 logic [3:0] RA1, RA2;
 
@@ -19,11 +19,11 @@ logic [N-1:0] PCPlus4, PC1, PCPlus8;
 
 Mux2 #(N) PCSrcMux(PCSrc, PCPlus4, Result, PC1);
 PC_Register PCReg(clk, rst, 1'b1, PC1, PC);
-full_adder_nb #(N) PCAdder1(PC, 13'd1, 1'b0, PCPlus4, cout1); 
-full_adder_nb #(N) PCAdder2(PCPlus4, 13'd1, 1'b0, PCPlus8, cout2);
+full_adder_nb #(N) PCAdder1(PC, 6'd1, 1'b0, PCPlus4, cout1); 
+full_adder_nb #(N) PCAdder2(PCPlus4, 6'd1, 1'b0, PCPlus8, cout2);
 Mux4 #(4) RegSrcMux0(RegSrc[1:0], Instr[19:16], 4'b1111, Instr[11:8], 0, RA1);
 Mux2 #(4) RegSrcMux1(RegSrc[2], Instr[3:0], Instr[15:12], RA2);
-RegisterFile #(32) RegFile(clk, rst, RegWrite, RA1, RA2, Instr[15:12], Result, PCPlus8, RD1, RD2);
+RegisterFile #(32) RegFile(clk, rst, RegWrite, RA1, RA2, Instr[15:12], Result, PCPlus8E, RD1, RD2);
 Extend Ext(ImmSrc, Instr[23:0], ExtImm);
 Mux2 #(32) SrcBMux(ALUSrc, RD2, ExtImm, SrcB);
 ALU #(32) Alu(RD1, SrcB, ALUControl, ALUResult, ALUFlags);
@@ -32,5 +32,6 @@ Mux2 #(32) MemtoRegMux(MemtoReg, ALUResult, ReadData, Result);
 assign AddrIM = PC; // Address memoria de instrucciones
 assign WriteData = RD2;
 assign Addr = ALUResult;
+assign PCPlus8E = {{26{PCPlus8[5]}},PCPlus8};
 
 endmodule  
